72004bd44574f264ae8b9cd9e83c30f0ec348b12
Torear <Staryd797@yandex.ru>
2021-05-06 19:07:26 +0300

    fix js

--- public/js/commonScripts.js
+++ public/js/commonScripts.js
@@ -491,3 +491,15 @@ function fileActionSHA(action = 0, file) {
         reader.readAsDataURL(file);
     }
 }
+
+function utf8_to_b64(str) {
+    return window.btoa(unescape(encodeURIComponent(str)));
+}
+
+function b64_to_utf8(str) {
+    try {
+        return decodeURIComponent(escape(window.atob(str)));
+    } catch (e) {
+        return "Error";
+    }
+}
